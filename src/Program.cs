b09e7cbeb2cca91926bc7b74f2efd748fd2adcf0
Martin Cornelius <62438208+MartinCornelius@users.noreply.github.com>
2024-02-28 12:22:44 +0100

    add: board print function

--- src/Program.cs
+++ src/Program.cs
@@ -4,28 +4,16 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        Console.Clear();
+
         Player player1 = new Player("Martim", Color.black);
         Player player2 = new Player("Martin", Color.white);
 
         Board board = new Board();
         board.InitBoard();
+        board.Print();
 
-        foreach(var item in board.GetBoardState())
-        {
-            foreach(var dingle in item){
-                Console.Write("{0} ", dingle.GetColor());
-            }
-            Console.Write("\n");
-        }
-        Console.WriteLine("_______________________");
-        board.UpdatePiecePosition(1, 1, player1);
-
-        foreach(var item in board.GetBoardState())
-        {
-            foreach(var dingle in item){
-                Console.Write("{0} ", dingle.GetColor());
-            }
-            Console.Write("\n");
-        }
+        board.MovePiece(1, 4, player1);
+        board.Print();
     }
 }
