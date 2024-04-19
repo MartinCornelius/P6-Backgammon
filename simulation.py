de6295420fe40d7afb9fe69e7723ce8cd96c0376
Martin Cornelius <62438208+MartinCornelius@users.noreply.github.com>
2024-04-19 11:16:56 +0200

    add: monte carlo

--- simulation.py
+++ simulation.py
@@ -2,49 +2,46 @@
 
 import random
 from board import Board
-from markovChain import MarkovChain
+from itertools import product
 
-def monte_carlo_simulation(num_simulations):
-    wins = [0, 0] # Number of wins for each player
+class MonteCarlo:
+    def __init__(self, board):
+        self.board = board
 
-    for _ in range(num_simulations):
-        if _ % 1000 == 0:
-            print(f"Simulation {_}")
+    def simulate_game(self, num_simulations):
+        wins = {(d1, d2): [0, 0] for d1, d2 in product(range(1, 7), repeat=2)}
 
-        init_state = {
-            'points': [
-                [5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 5, 2, 1]
-            ],
-            'bar': [0,0],
-            'borne_off': [0,0],
-            'current_player': 0
-        }
-        board = Board(init_state)
-        markov_chain = MarkovChain(board)
-        win_probabilities = markov_chain.calculate_win_probability()
-        print(win_probabilities)
+        for _ in range(num_simulations):
+            board_copy = self.board.copy()
 
-        # best_roll = max(win_probabilities, key=win_probabilities.get)
-        # d1, d2 = best_roll
+            while not board_copy.is_game_over():
+                d1, d2 = board_copy.roll_dice()
+                legal_moves = board_copy.get_legal_moves(board_copy.current_player, (d1, d2))
+                if not legal_moves:
+                    board_copy.current_player = 1 - board_copy.current_player
+                    continue
+                move = random.choice(legal_moves)
+                board_copy.move_piece(board_copy.current_player, move[0], move[1])
+                board_copy.current_player = 1 - board_copy.current_player
 
-    #     while not board.is_game_over():
-    #         d1, d2 = board.roll_dice()
-    #         legal_moves = board.get_legal_moves(board.current_player, (d1, d2))
-    #         if not legal_moves:
-    #             board.current_player = 1 - board.current_player
-    #             continue
-    #         move = random.choice(legal_moves)
-    #         board.move_piece(board.current_player, move[0], move[1])
-    #         board.current_player = 1 - board.current_player
+            winner = 0 if board_copy.borne_off[0] == 15 else 1
+            wins[(d1, d2)][winner] += 1
 
-    #     winner = 0 if board.borne_off[0] == 15 else 1
-    #     wins[winner] += 1
+        return wins
 
-    # return wins
+    def calculate_win_probability(self):
+        num_simulations = 10000
+        wins = self.simulate_game(num_simulations)
 
-num_simulations = 1
-results = monte_carlo_simulation(num_simulations)
+        win_probabilities = {}
+        for dice_roll, win_counts in wins.items():
+            total_wins = win_counts[0] + win_counts[1]
+            if total_wins > 0:
+                win_probabilities[dice_roll] = win_counts[0] / total_wins
 
-# print(f"Player 1 wins: {results[0]/num_simulations*100:.1f}% with {results[0]} games")
-# print(f"Player 2 wins: {results[1]/num_simulations*100:.1f}% with {results[1]} games")
+        return win_probabilities
+
+if __name__ == '__main__':
+    board = Board()
+    mc = MonteCarlo(board)
+    print(mc.calculate_win_probability())
