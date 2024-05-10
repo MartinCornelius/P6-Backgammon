2b46377eb9b5b781649eb58d710717cc7f16dd7f
Lucas2k <lucasmartin2k@gmail.com>
2024-05-10 15:45:46 +0200

    updated heuristics

--- heuristics.py
+++ heuristics.py
@@ -42,15 +42,67 @@ def keep_pieces_safe(possible_moves, current_board):
 	return rand_choice(possible_moves, None)
 
 def hit_enemy_pieces(possible_moves, current_board):
-	if current_board.current_player == 0:
-		for move in possible_moves:
-			if current_board.points[1][move[1]] == 1:
-				return move
-	else:
-		for move in possible_moves:
-			if current_board.points[0][move[1]] == 1:
-				return move
+	for move in possible_moves:
+		if current_board.points[1 - current_board.current_player][move[1]] == 1:
+			return move
 	return rand_choice(possible_moves, None)
 
 def bear_off_first(possible_moves, current_board):
 	pass
+
+def dan_heuristic(possible_moves, current_board):
+	furthest_move = None
+	unchanged_move = None
+	#Check if hit move is possible.
+	if current_board.current_player == 0:
+		for move in possible_moves:
+			if current_board.points[1 - current_board.current_player][move[1]] == 1:
+				if furthest_move[0] < move[0]:
+					furthest_move = move
+				elif furthest_move[0] == move[0] and furthest_move[2] < move[2]:
+					furthest_move = move
+	else:
+		for move in possible_moves:
+			if current_board.points[1 - current_board.current_player][move[1]] == 1:
+				if furthest_move[0] > move[0]:
+					furthest_move = move
+				elif furthest_move[0] == move[0] and furthest_move[2] < move[2]:
+					furthest_move = move
+
+	if current_board.current_player == 0:
+		if furthest_move == unchanged_move:
+			#Check if safe move is possible.
+			for move in possible_moves:
+				if current_board.points[current_board.current_player][move[1]] > 0:
+					if furthest_move[0] < move[0]:
+						furthest_move = move
+					elif furthest_move[0] == move[0] and furthest_move[2] < move[2]:
+						furthest_move = move
+	else:
+		if furthest_move == unchanged_move:
+			#Check if safe move is possible.
+			for move in possible_moves:
+				if current_board.points[current_board.current_player][move[1]] > 0:
+					if furthest_move[0] > move[0]:
+						furthest_move = move
+					elif furthest_move[0] == move[0] and furthest_move[2] < move[2]:
+						furthest_move = move
+
+	if furthest_move == unchanged_move:
+		#Find furthest move.
+		furthest_move = possible_moves[0]
+		if current_board.current_player == 0:
+			for move in possible_moves:
+				if furthest_move[0] < move[0]:
+					furthest_move = move
+				elif furthest_move[0] == move[0] and furthest_move[2] < move[2]:
+					furthest_move = move
+		else:
+			for move in possible_moves:
+				if furthest_move[0] > move[0]:
+					furthest_move = move
+				elif furthest_move[0] == move[0] and furthest_move[2] < move[2]:
+					furthest_move = move
+
+	return furthest_move
+
