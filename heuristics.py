a8aff78254822c886186c3eb0907df0a1c766ae9
Lucas2k <lucasmartin2k@gmail.com>
2024-05-10 13:47:53 +0200

    furthest and shortest

--- heuristics.py
+++ heuristics.py
@@ -7,18 +7,30 @@ def move_furthest_first(possible_moves, current_board):
 	furthest_move = possible_moves[0]
 	for move in possible_moves:
 		if current_board.current_player == 0:
-			furthest_move = move if furthest_move[0] > move[0] else furthest_move
+			if furthest_move[0] < move[0]:
+				furthest_move = move
+			elif furthest_move[0] == move[0] and furthest_move[2] < move[2]:
+				furthest_move = move
 		else:
-			furthest_move = move if furthest_move[0] < move[0] else furthest_move
-	print(possible_moves)
-	print(furthest_move)
+			if furthest_move[0] > move[0]:
+				furthest_move = move
+			elif furthest_move[0] == move[0] and furthest_move[2] < move[2]:
+				furthest_move = move
 	return furthest_move
 
 def move_closest_first(possible_moves, current_board):
 	closest_move = possible_moves[0]
 	for move in possible_moves:
 		if current_board.current_player == 0:
-			closest_move = move if closest_move[0] < move[0] else closest_move
+			if closest_move[0] > move[0]:
+				closest_move = move
+			elif closest_move[0] == move[0] and closest_move[2] < move[2]:
+				closest_move = move
+		else:
+			if closest_move[0] < move[0]:
+				closest_move = move
+			elif closest_move[0] == move[0] and closest_move[2] < move[2]:
+				closest_move = move
 	return closest_move
 
 def keep_pieces_safe():
